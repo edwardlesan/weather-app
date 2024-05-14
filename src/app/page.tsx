4d55ee564d1702dee31287844aa349fe838f19e0
Eduardo <edwardsendrea1@gmail.com>
2024-05-15 01:01:29 +0300

    Implement Switch Case Function for Background Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,25 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
 import WeatherCard from "@/components/WeatherCard";
+import Image from "next/image";
 
 export interface WeatherDataProps {
   main: {
     temp: number;
-    feels_like: number;
     humidity: number;
     pressure: number;
   };
+  timezone: number;
   name: string;
   weather: {
     main: string;
     description: string;
+    icon: string;
   }[];
 }
 
 export default function Home() {
-  const [city, setCity] = useState("dubai");
+  const [city, setCity] = useState("tokyo");
   const [weather, setWeather] = useState<WeatherDataProps | null>(null);
   const [loading, setLoading] = useState(false);
 
@@ -37,12 +39,58 @@ export default function Home() {
       setLoading(false);
     }
   };
+  console.log(weather);
+
+  function selectImage(
+    weatherDescription?: string,
+    timezoneOffsetSeconds?: number
+  ): string {
+    if (timezoneOffsetSeconds === undefined) {
+      timezoneOffsetSeconds = 0;
+    }
+
+    const currentTime = new Date(Date.now() + timezoneOffsetSeconds * 1000);
+    const hour = currentTime.getHours();
+    const isDayTime = hour >= 6 && hour < 18;
+
+    switch (weatherDescription) {
+      case "clear sky":
+        return isDayTime ? "/clear_sky_day.jpg" : "/clear_sky_night.jpg";
+      case "few clouds":
+      case "scattered clouds":
+      case "broken clouds":
+        return isDayTime ? "/day_clouds.jpg" : "/night_clouds.jpg";
+      case "shower rain":
+      case "rain":
+        return isDayTime ? "/rain_day.jpg" : "/rain_night.jpg";
+      case "thunderstorm":
+        return isDayTime ? "/thunderstorm_day.jpg" : "/thunderstorm_night.jpg";
+      case "snow":
+        return isDayTime ? "/snow_day.jpg" : "/snow_night.jpg";
+      case "mist":
+        return isDayTime ? "/mist_day.jpg" : "/mist_night.jpg";
+      default:
+        return isDayTime ? "/clear_sky_day.jpg" : "/clear_sky_night.jpg";
+    }
+  }
 
   return (
-    <div className="w-full h-screen p-10 flex gap-10 items-center justify-center">
-      <Button onClick={fetchWeather}>Test</Button>
+    <div className="relative flex flex-col w-full h-dvh px-20">
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-10 bg-black/20 backdrop-blur-sm" />
+      <Image
+        src={selectImage(weather?.weather[0].description, weather?.timezone)}
+        layout="fill"
+        className="object-cover z-0"
+        alt="background-image"
+      />
 
-      <WeatherCard data={weather} />
+      <div className="relative z-20 py-4">
+        <Button onClick={fetchWeather}>Test</Button>
+      </div>
+
+      <div className="relative z-20 flex-1 flex items-center w-full justify-end">
+        <WeatherCard data={weather} />
+      </div>
     </div>
   );
 }
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Card } from "./ui/card";
 import { WeatherDataProps } from "@/app/page";
+import Image from "next/image";
+import { LuDroplet } from "react-icons/lu";
+import { LuThermometer } from "react-icons/lu";
+import { LuArrowDownNarrowWide } from "react-icons/lu";
 
 interface WeatherCardProps {
   data: WeatherDataProps | null;
@@ -8,19 +12,35 @@ interface WeatherCardProps {
 
 const WeatherCard: React.FC<WeatherCardProps> = ({ data }) => {
   return (
-    <Card className="p-4">
+    <Card className="px-6 py-4 w-64 border-2">
       {data ? (
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-16">
           <div className="flex flex-col gap-1">
             <h1 className="text-3xl font-semibold">{data.name}</h1>
-            <p className="text-md font-medium text-gray-600">
-              {data.weather[0].main}
-            </p>
+            <div className="flex w-full items-center">
+              <p className="text-lg font-medium text-gray-600">
+                {data.weather[0].main}
+              </p>
+              <Image
+                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt="/"
+                width="48"
+                height="48"
+              />
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <p>Temperature: {data.main.temp}°C</p>
-            <p>Humidity: {data.main.humidity}%</p>
-            <p>Pressure: {data.main.pressure} hPa</p>
+          <div className="flex flex-col gap-2">
+            <p className="items-center flex gap-1">
+              <LuThermometer stroke="black" size={24} />
+              {data.main.temp} °C
+            </p>
+            <p className="items-center flex gap-1">
+              <LuDroplet stroke="black" size={24} /> {data.main.humidity} %
+            </p>
+            <p className="items-center flex gap-1">
+              <LuArrowDownNarrowWide stroke="black" size={24} />
+              {data.main.pressure} hPa
+            </p>
           </div>
         </div>
       ) : (
